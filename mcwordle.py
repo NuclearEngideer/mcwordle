c2cdce75f58b82abc8fb9dc8e97b6db3aa2347ac
Ave <56891155+NuclearEngideer@users.noreply.github.com>
2022-09-10 02:35:13 -0600

    Preliminary completion

--- mcwordle.py
+++ mcwordle.py
@@ -1,74 +1,103 @@
 from time import time
 import random
 import json
+import numpy as np
+from colorama import Back, Style
 
-guess = []
-answer='piton'
+num_guesses=np.array([])
 
-solutionjson=open('data.json')
-fiveletterwords=json.load(solutionjson)
-
+words=open('data.json')
+fiveletterwords=json.load(words)
 solutions=fiveletterwords['solutions']
 allowedWords=fiveletterwords['valid_words']
 
-letters={'1': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-         '2': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-         '3': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-         '4': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-         '5': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']}
-
-def checkWord(guess, answer):
+def checkWord(guess, answer, letters):
   res=[0,0,0,0,0]
-  indeces=[]
   i=0
-  g=-1
+  g=[]
   while i<5:
+    indeces=[]
     # If guess is in word, record location(s)
     if guess[i] in answer:
-      g = answer.index(guess[i])
+      indeces=getIndexes(guess[i], answer)
+      for index in indeces:
+        # First set yellows:
+        if res[i] != 2:
+          res[i]=1
+        if i==index:
+          res[i]=2
     # Remove letters from the guess set if they're not in the word at all
     else:
+      indeces=[-1]
       for j in range(1,6):
         if guess[i] in letters[str(j)]:
           letters[str(j)].pop(letters[str(j)].index(guess[i]))
-    # Figure out if any letters are in the proper position, or are in the word and in the wrong place
-    indeces=getIndexes(guess[i], answer)
-    # First set yellows:
-    if g in (indeces):
-      res[i]=1
-    # Set Greens
-    if g==i:
-      res[i]=2
+
     i=i+1
   return res
 
 def getIndexes(guessed_letter,answer):
   return [i for i, x in enumerate(answer) if x == guessed_letter]
 
-def popYellows(guess,res):
+def popYellows(guess,res,yellow_letters,letters):
   i=1
   while i<6:
     if res[i-1] == 1:
-      letters[str(i)].pop(letters[str(i)].index(guess[i]))
+      letters[str(i)].pop(letters[str(i)].index(guess[i-1]))
+      yellow_letters.append(guess[i-1])
     i+=1
+  return yellow_letters
 
 def rollLetter(letterset):
   return letterset[random.randrange(0,len(letterset))]
 
-guess=list('crate')
-guesscount=1
-result=checkWord(guess, answer)
-print(result)
-while result != [2,2,2,2,2]:
-  i=0
+def newWord(guess, result, letters):
   for i in range(1,6):
     if result[i-1] == 1 or result[i-1] == 0:
       guess[i-1] = rollLetter(letters[str(i)])
-    print(guess)
-  if guess in allowedWords or guess in solutions:
-    result = checkWord(guess,answer)
-    print(result)
+  return guess
+
+def printRes(guess, result):
+  # let's color the letters!
+  for i in range(0,5):
+    if result[i]==1:
+      print(Back.YELLOW + guess[i], end='')
+    elif result[i]==2:
+      print(Back.GREEN + guess[i], end='')
+    else:
+      print(Back.BLACK + guess[i], end='')
+  print(Style.RESET_ALL)
+
+def main(seed, answer):
+  guess=seed
+  letters={'1': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+           '2': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+           '3': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+           '4': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+           '5': ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']}
+  guesscount=1
+  yellow_letters=[]
+  result=checkWord(guess, answer, letters)
+  printRes(guess, result)
+  yellow_letters=popYellows(guess, result, yellow_letters, letters)
+  while result != [2,2,2,2,2]:
+    guess=newWord(guess, result, letters)
+    while ''.join(guess) not in allowedWords and ''.join(guess) not in solutions:
+      guess=newWord(guess, result, letters)
+      while len(set(yellow_letters)-set(guess)) > 0:
+        guess=newWord(guess,result,letters)
+    result = checkWord(guess, answer, letters)
+    yellow_letters=popYellows(guess, result,yellow_letters,letters)
+    printRes(guess, result)
     guesscount+=1
-  
-print(guesscount)
-# def cleanLetters(guess[i])
+  print(str(guesscount)+'/6')
+  return guesscount
+
+for ans in solutions[0:99]:
+  num_guesses=np.append(num_guesses, main(list('crate'), ans))
+
+print(f'\n----STATISTICS----\n'
+      f'Max={num_guesses.max()}\n'
+      f'Min={num_guesses.min()}\n'
+      f'Avg={np.average(num_guesses)}\n'
+      f'std={np.std(num_guesses)}\n')
